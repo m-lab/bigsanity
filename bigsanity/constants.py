63dd93ab9ecad5e464efdca08161061156821f51
Michael Lynch <mtlynch@google.com>
2015-12-07 17:02:34 -0500

    Adding table name generator
    Adding a module to generate M-Lab BigQuery table names for given
    projects or given time ranges.

---
+++ bigsanity/constants.py
@@ -0,0 +1,27 @@
+# Copyright 2015 Measurement Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Project IDs from:
+# https://github.com/m-lab/mlab-wikis/blob/master/BigQueryMLabDataset.md
+PROJECT_ID_NDT = 0
+PROJECT_ID_NPAD = 1
+PROJECT_ID_SIDESTREAM = 2
+PROJECT_ID_PARIS_TRACEROUTE = 3
+
+PROJECT_NAME_NDT = 'ndt'
+PROJECT_NAME_NPAD = 'npad'
+PROJECT_NAME_SIDESTREAM = 'sidestream'
+PROJECT_NAME_PARIS_TRACEROUTE = 'paris_traceroute'
+
+MLAB_EPOCH = 1234310400  # 2009-02-11, date of first M-Lab test
---
+++ tests/test_table_names.py
@@ -0,0 +1,91 @@
+# Copyright 2015 Measurement Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import os
+import sys
+import unittest
+
+sys.path.insert(1, os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '../bigsanity')))
+import constants
+import table_names
+
+
+class TableNamesTest(unittest.TestCase):
+
+    def test_per_project_table(self):
+        """Generate valid table names for valid project IDs."""
+        self.assertEqual(
+            'plx.google:m_lab.ndt.all',
+            table_names.per_project_table(constants.PROJECT_ID_NDT))
+        self.assertEqual(
+            'plx.google:m_lab.npad.all',
+            table_names.per_project_table(constants.PROJECT_ID_NPAD))
+        self.assertEqual(
+            'plx.google:m_lab.sidestream.all',
+            table_names.per_project_table(constants.PROJECT_ID_SIDESTREAM))
+        self.assertEqual('plx.google:m_lab.paris_traceroute.all',
+                         table_names.per_project_table(
+                             constants.PROJECT_ID_PARIS_TRACEROUTE))
+
+    def test_per_project_table_rejects_invalid_projects(self):
+        """Reject invalid M-Lab project IDs."""
+        with self.assertRaises(ValueError):
+            table_names.per_project_table(500)
+
+    def test_monthly_tables_rejects_invalid_dates(self):
+        """Reject invalid table dates."""
+        # 2009-02-01 is before M-Lab epoch.
+        with self.assertRaises(ValueError):
+            table_names.monthly_tables(
+                datetime.datetime(2009, 2, 1), datetime.datetime(2009, 3, 1))
+        # It should never be possible to generate table names for future dates.
+        with self.assertRaises(ValueError):
+            table_names.monthly_tables(
+                datetime.datetime.now(),
+                datetime.datetime.now() + datetime.timedelta(days=1))
+
+    def test_monthly_tables(self):
+        """Generate monthly table names for valid date range."""
+        self.assertSequenceEqual(
+            ('plx.google:m_lab.2009_02_fast.all',
+             'plx.google:m_lab.2009_03_fast.all'), table_names.monthly_tables(
+                 datetime.datetime(2009, 2, 11), datetime.datetime(2009, 3, 1)))
+
+        # Including the 1-day buffer, 2012-01-01 spills over into the previous
+        # month's table.
+        self.assertSequenceEqual(
+            ('plx.google:m_lab.2011_12_fast.all',
+             'plx.google:m_lab.2012_01_fast.all',
+             'plx.google:m_lab.2012_02_fast.all'), table_names.monthly_tables(
+                 datetime.datetime(2012, 1, 1), datetime.datetime(2012, 2, 1)))
+
+    def test_legacy_monthly_tables(self):
+        """Generate legacy monthly table names for valid date range."""
+        self.assertSequenceEqual(
+            ('plx.google:m_lab.2009_02.all',
+             'plx.google:m_lab.2009_03.all'), table_names.legacy_monthly_tables(
+                 datetime.datetime(2009, 2, 11), datetime.datetime(2009, 3, 1)))
+
+        # Including the 1-day buffer, 2012-01-01 spills over into the previous
+        # month's table.
+        self.assertSequenceEqual(
+            ('plx.google:m_lab.2011_12.all', 'plx.google:m_lab.2012_01.all',
+             'plx.google:m_lab.2012_02.all'), table_names.legacy_monthly_tables(
+                 datetime.datetime(2012, 1, 1), datetime.datetime(2012, 2, 1)))
+
+
+if __name__ == '__main__':
+    unittest.main()
